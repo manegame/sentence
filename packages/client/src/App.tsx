b8162e6150ca77ee044c1df3bf3aa44ac191a3e9
Jacob Willemsma <jacob.willemsma@gmail.com>
2023-04-01 16:57:22 +0200

    Clean up intro logic

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,45 +1,25 @@
 import { useState } from "react";
 import styled from "styled-components";
-import Typewriter from "typewriter-effect";
 
-// import { Introduction } from "./Introduction";
+import { Introduction } from "./Introduction";
 import { Write } from "./Write";
 import { Sentences } from "./Sentences";
 
 export const App = () => {
-  // const [showIntro, setShowIntro] = useState(true);
+  const [showIntro, setShowIntro] = useState(true);
 
   // // setTimeout(() => setShowIntro(false), 3000)
 
   return (
     <FullHeightDiv>
-      <Typewriter
-        options={{
-          delay: 30,
-          autoStart: true,
-        }}
-        onInit={(typewriter) => {
-          typewriter
-            .typeString(
-              '<p style="margin: 0px; color: white">They are playing a game.<p/>'
-            )
-            .pauseFor(100)
-            .typeString(
-              '<p style="margin: 0px; color: white">They are playing at not playing a game.<p/>'
-            )
-            .pauseFor(100)
-            .typeString(
-              '<p style="margin: 0px; color: white">If I show them I see they are, I shall break the rules and they will punish me.<p/>'
-            )
-            .pauseFor(500)
-            .typeString(
-              '<b style="margin: 0px; color: white">I must play their game, of not seeing I see the game...</b>'
-            )
-            .start();
-        }}
-      />
-      {/* <Write />
-      <Sentences /> */}
+      {showIntro ? (
+        <Introduction setShowIntro={setShowIntro} />
+      ) : (
+        <>
+          <Write />
+          <Sentences />
+        </>
+      )}
     </FullHeightDiv>
   );
 };
