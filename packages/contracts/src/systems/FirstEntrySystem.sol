5a736557fd715edb41fa634961b4d2f39a381464
Jacob Willemsma <jacob.willemsma@gmail.com>
2023-04-02 15:24:15 +0200

    Update FirstEntrySystem to pick a random prompt to start on chain

--- packages/contracts/src/systems/FirstEntrySystem.sol
+++ packages/contracts/src/systems/FirstEntrySystem.sol
@@ -7,12 +7,29 @@ import { Entry } from "../tables/Entry.sol";
 bytes32 constant SingletonKey = bytes32(uint256(0x060D));
 
 contract FirstEntrySystem is System {
+    function getRandomIndex(uint256 arrayLength) public view returns (uint256) {
+        bytes32 blockHash = blockhash(block.number - 1);
+        uint256 randomIndex = uint256(blockHash) % arrayLength;
+        return randomIndex;
+    }
+
+
     function createStory() public {
         address proposer = _msgSender(); // IMPORTANT: always refer to the msg.sender using the _msgSender() function
         
         bytes32 key = bytes32(keccak256(abi.encodePacked(block.number, proposer, gasleft()))); // creating a random key for the record
         address parentAddress = address(0);
-        string memory sentence = "Imagine a nation based upon water...";
+
+        // TODO: Add more prompts
+        string[] memory storyBeginnings = new string[](5);
+        storyBeginnings[0] = "The story so far: in the beginning, the universe was created. This has made a lot of people very angry and been widely regarded as a bad move.";
+        storyBeginnings[1] = "Mother died today. Or maybe, yesterday; I can't be sure.";
+        storyBeginnings[2] = "Ships at a distance have every man's wish on board.";
+        storyBeginnings[3] = "It was a bright cold day in April, and the clocks were striking thirteen.";
+        storyBeginnings[4] = "Imagine a nation based upon water...";
+
+
+        string memory sentence = storyBeginnings[getRandomIndex(5)];
 
         Entry.set(key, parentAddress, proposer, sentence);
     }
